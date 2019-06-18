084315c4d2a376455f42ea9e29160906effdc29b
erikterminator <erikhansenmujica@gmail.com>
2019-06-18 19:02:39 -0300

    base de datos con cosas

--- front/react/index.js
+++ front/react/index.js
@@ -6,19 +6,11 @@ import { Provider } from 'react-redux';
 import store from '../store/index';
 
 ReactDOM.render(
-<<<<<<< HEAD
-    <Provider store={store}>
-    <BrowserRouter>
-      <Route path="/" component={Main} />
-    </BrowserRouter>
-    </Provider>,
-=======
   <Provider store={store}>
     <BrowserRouter>
       <Route path="/" component={Main} />
     </BrowserRouter>
   </Provider>,
->>>>>>> 605701f567791ca3b0e013ee1248e253a82f0364
     document.getElementById('app')
 
 );
