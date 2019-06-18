11910e3d7c2c8baff7c113033930e6ce9a32cd73
Rosanavpu <rosanap06@gmail.com>
2019-06-18 19:07:47 -0300

    Hice poquito de Home

--- front/react/components/Home.jsx
+++ front/react/components/Home.jsx
@@ -1,32 +1,32 @@
-import React from "react"
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import IconButton from "@material-ui/core/IconButton";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
 //import tileData from './tileData';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper
   },
   gridList: {
-    flexWrap: 'nowrap',
+    flexWrap: "nowrap",
     // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: 'translateZ(0)',
+    transform: "translateZ(0)"
   },
   title: {
-    color: theme.palette.primary.light,
+    color: theme.palette.primary.light
   },
   titleBar: {
     background:
-      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-  },
+      "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)"
+  }
 }));
 
 export default function SingleLineGridList() {
@@ -34,30 +34,30 @@ export default function SingleLineGridList() {
 
   return (
     <div className={classes.root}>
-      <GridList className={classes.gridList} cols={2.5}>
-       
-          <GridListTile>
-            {/* <img src={} alt={tile.title} /> */}
-            <GridListTileBar/>
 
-                <IconButton >
-                  <StarBorderIcon  />
+    <h1 className={classes.title}>
+      Estoy entrando
+    </h1>
+    {/* <GridList className={classes.gridList} cols={2.5}>
+
+        {tileData.map(tile => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={
+                <IconButton aria-label={`star ${tile.title}`}>
+                  <StarBorderIcon className={classes.title} />
                 </IconButton>
-            
-            
+              }
+            />
           </GridListTile>
-      
-      </GridList>
+        ))}
+      </GridList> */}
     </div>
   );
 }
-
-// export default ()=>{
-//     return (
-//         <div style={styles.contentMessage}>
-//             HOLAAAAAAA
-//         </div>
-//     )
-
-
-// }
