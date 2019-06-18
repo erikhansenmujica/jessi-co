f08450a3a15045ae2579bbb10d414553e6ff4e8e
erikterminator <erikhansenmujica@gmail.com>
2019-06-18 17:20:34 -0300

    fixed axios gets

--- front/store/actions/getProducts.js
+++ front/store/actions/getProducts.js
@@ -1,4 +1,4 @@
-import {ADD_PRODUCTS} from "../../utils/constants"
+import {ADD_PRODUCT, ADD_PRODUCTS} from "../../utils/constants"
 import Axios from "axios";
 import store from "../../store/index"
 
@@ -7,10 +7,19 @@ const addProducts = (products)=>({
     products
 })
 
+const addProduct = (product)=>({
+    type:ADD_PRODUCT,
+    product
+})
+
 export const fetchProductsByName =(name)=> 
-Axios.get(`/api/products${name}`)
+Axios.get(`/api/products/${name}`)
 .then(products=>store.dispatch(addProducts(products)))
 
 export const fetchProductsByCat =(cat)=> 
-Axios.get(`/api/products${cat}`)
+Axios.get(`/api/products/${cat}`)
 .then(products=>store.dispatch(addProducts(products)))
+
+export const fetchProductsById =(id)=> 
+Axios.get(`/api/products/${id}`)
+.then(product=>store.dispatch(addProduct(product)))
--- front/utils/constants.js
+++ front/utils/constants.js
@@ -1,5 +1,6 @@
 //constantes para storear cosas:
 const ADD_PRODUCTS ="ADD_PRODUCTS"
+const ADD_PRODUCT="ADD_PRODUCT"
 const ADD_CART ="ADD_CART"
 
-export {ADD_PRODUCTS, ADD_CART}
+export {ADD_PRODUCTS, ADD_CART,ADD_PRODUCT}
