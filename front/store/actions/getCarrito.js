6b301bead68e2befd3517a426ab89708450809dd
Federico Morales Otero <fmoralesotero@gmail.com>
2019-06-24 18:38:50 -0300

    Sigo con carrito

--- front/store/actions/getCarrito.js
+++ front/store/actions/getCarrito.js
@@ -1,22 +1,27 @@
-import {SET_CART, REM_CART} from "../../utils/constants"
+import { SET_CART, REM_CART, QUANTITY_DOWN, QUANTITY_UP } from "../../utils/constants"
 import Axios from "axios";
 import store from "../../store/index"
 
-export const setCart = (carrito)=>{
-    return{
-    type: SET_CART,
-    carrito
-}}
-export const remCart = (carrito)=>({
+export const setCart = (carrito) => {
+    return {
+        type: SET_CART,
+        carrito
+    }
+}
+export const remCart = (carrito) => ({
     type: REM_CART,
     carrito
 })
 
+export const quantityUp = (id) => ({
+    type: QUANTITY_UP,
+    id
+})
 
 
-export const fetchCarrito = (user)=> 
-Axios.get(`/api/carrito/${user.id}`)  
-.then(carrito=>store.dispatch(setCart(carrito)))
+export const fetchCarrito = (user) =>
+    Axios.get(`/api/carrito/${user.id}`)
+        .then(carrito => store.dispatch(setCart(carrito)))
 
 
 
