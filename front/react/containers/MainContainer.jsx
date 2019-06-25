af1a71ca22353e53d600d223bb013e8fde3ce69b
erikterminator <erikhansenmujica@gmail.com>
2019-06-25 18:44:47 -0300

    skere

--- front/react/containers/MainContainer.jsx
+++ front/react/containers/MainContainer.jsx
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import Main from "../components/Main";
 import { connect } from "react-redux";
 import { remCart } from "../../store/actions/getCarrito";
+import { addProduct, addProducts } from "../../store/actions/getProducts";
 import { fetchLoggedUser } from "../../store/actions/logUser";
-import Axios from "axios"
 
 class MainContainer extends Component {
   constructor(props) {
@@ -12,12 +12,17 @@ class MainContainer extends Component {
  
  componentDidMount(){
     !sessionStorage.getItem('product')&&sessionStorage.setItem('product',JSON.stringify(this.props.carrito))
+    !sessionStorage.getItem('products')&&sessionStorage.setItem('products',JSON.stringify(this.props.products))
+    !sessionStorage.getItem('selectedProduct')&&sessionStorage.setItem('selectedProduct',JSON.stringify(this.props.product))
     if(!this.props.carrito[0]&&sessionStorage.getItem('product')[0])this.props.addOldCart(JSON.parse(sessionStorage.getItem('product')));
     this.props.addLoggedUser()
+    if(!this.props.products[0])this.props.addOldProducts(JSON.parse(sessionStorage.getItem("products")))
+    if(!this.props.product.name)this.props.addOldProduct(JSON.parse(sessionStorage.getItem("selectedProduct")))
  }
  componentDidUpdate(){
     this.props.carrito[0]&&sessionStorage.setItem("product",JSON.stringify(this.props.carrito))
-
+    this.props.products[0]&&sessionStorage.setItem("products",JSON.stringify(this.props.products))
+    this.props.product.name&&sessionStorage.setItem("selectedProduct",JSON.stringify(this.props.product))
  }
   render() {
     return (
@@ -26,12 +31,16 @@ class MainContainer extends Component {
   }
 }
 
-const mapStateToProps = ({ carrito }) => ({
-    carrito: carrito.products
+const mapStateToProps = ({ carrito, products }) => ({
+    carrito: carrito.products,
+    product: products.product,
+    products: products.products
   });
   const mapDispatchToProps = dispatch => {
     return {
       addOldCart: product => dispatch(remCart(product)),
+      addOldProduct: product => dispatch(addProduct(product)),
+      addOldProducts: product => dispatch(addProducts(product)),
       addLoggedUser:fetchLoggedUser
     };
   };
